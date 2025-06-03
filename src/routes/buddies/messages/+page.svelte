af51e34a12893ecf807aff6344053fcde8bafd76
Bowen Zhu <bowen.zhu@me.com>
2025-06-03 22:52:10 +0100

    add some leftover changes

--- src/routes/buddies/messages/+page.svelte
+++ src/routes/buddies/messages/+page.svelte
@@ -3,16 +3,8 @@
 </script>
 
 <div class="w-full flex flex-col items-center gap-y-3">
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
-	<Chat></Chat>
+	<Chat name="Jim"></Chat>
+	<Chat name="Other guy"></Chat>
+	<Chat name="Thingy"></Chat>
+	<Chat name="Simone"></Chat>
 </div>
--- src/routes/buddies/messages/chat.svelte
+++ src/routes/buddies/messages/chat.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	import TablerUserCircle from 'virtual:icons/tabler/user-circle';
+
+	interface Props {
+		name?: string;
+		preview?: string;
+	}
+
+	let {
+		name = 'Bob',
+		preview = "Skibidi lorem ipsum dolor sit amet, gyatt consectetur adipiscing elit, no cap rizzler sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bussin' ut enim ad minim veniam, only in Ohio quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Sussy imposter duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Sigma male excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Poggers livvy dunne rizzed up baby gronk, fr fr."
+	}: Props = $props();
 </script>
 
 <button
@@ -9,15 +19,7 @@
 		<div class="col-span-1">
 			<TablerUserCircle></TablerUserCircle>
 		</div>
-		<p class="col-span-5 text-left">Bob</p>
+		<p class="col-span-5 text-left">{name}</p>
 	</div>
-	<div class="h-10 text-ellipsis overflow-hidden block text-left">
-		Skibidi lorem ipsum dolor sit amet, gyatt consectetur adipiscing elit, no cap rizzler sed do
-		eiusmod tempor incididunt ut labore et dolore magna aliqua. Bussin' ut enim ad minim veniam,
-		only in Ohio quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-		Sussy imposter duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-		fugiat nulla pariatur. Sigma male excepteur sint occaecat cupidatat non proident, sunt in culpa
-		qui officia deserunt mollit anim id est laborum. Poggers livvy dunne rizzed up baby gronk, fr
-		fr.
-	</div>
+	<div class="h-10 text-ellipsis overflow-hidden block text-left">{preview}</div>
 </button>
