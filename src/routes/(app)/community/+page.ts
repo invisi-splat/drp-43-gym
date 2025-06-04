7ab9a26be490f88d7e2039bfe377951d5797c4a4
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2025-06-04 23:55:59 +0100

    format page.ts

--- src/routes/(app)/community/+page.ts
+++ src/routes/(app)/community/+page.ts
@@ -4,7 +4,7 @@ import type { PageLoad } from './$types';
 import type { WorkoutComponent } from '$lib/types/app';
 
 export const load: PageLoad = async () => {
-    console.log(">>>>>>>>>RUNNING page.ts load functon")
+	console.log('>>>>>>>>>RUNNING page.ts load functon');
 	const { data, error } = await supabase
 		.from('workouts')
 		.select('id, name, age, skill, "isFriend", regimen, "desc", location, "dateTime"');
@@ -15,17 +15,17 @@ export const load: PageLoad = async () => {
 	}
 
 	// Map to WorkoutComponent array
-	const workouts: WorkoutComponent[] = data.map(row => ({
+	const workouts: WorkoutComponent[] = data.map((row) => ({
 		name: row.name, // Placeholder
-		age: row.age,              // Placeholder
-		skill: row.skill,      // Placeholder
-		isFriend: row.isFriend,      // Placeholder
-		regimen: row.regimen,   // Placeholder
+		age: row.age, // Placeholder
+		skill: row.skill, // Placeholder
+		isFriend: row.isFriend, // Placeholder
+		regimen: row.regimen, // Placeholder
 		desc: row.desc,
-		location: row.location,      // Placeholder
-		dateTime: row.dateTime, // Placeholder
+		location: row.location, // Placeholder
+		dateTime: row.dateTime // Placeholder
 	}));
 
-    console.log(">>>>>>>>returning workouts: ", workouts)
+	console.log('>>>>>>>>returning workouts: ', workouts);
 	return { workouts };
 };
