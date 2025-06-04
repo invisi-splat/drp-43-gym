aaefe498fead98ded95719550e20cf8ec11f9c0e
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2025-06-04 11:10:14 +0100

    remove unused induction types

--- src/lib/types/induction.ts
+++
@@ -1,3 +0,0 @@
-export enum Day { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }
-export const trainingGoals = ["Weight loss", "Muscle gain", "General fitness"]
-export const machineTypes = ["Cardio machines", "Dumbbells/Barbells", "Weight machines"]
