3c041aea0e594eb9a8478ab67a838eb729d83627
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2025-06-04 12:02:45 +0100

    add dependencies, refactor types, and add + button

--- src/lib/types/app.ts
+++ src/lib/types/app.ts
@@ -1,10 +1,15 @@
-interface WorkoutComponent {
-  name: string,
-  age: number,
-  skill: string, // TODO change this to an enum of some sort (e.g. novice, experienced, ...)
-  isFriend: boolean,
-  regiment: string, // TODO change this to an enum of some sort (e.g. legs, chest, back, ...)
-  desc: string,
-  location: string,
-  dateTime: Date
+interface WorkoutProps {
+	regimen: string; // TODO change this to an enum of some sort (e.g. legs, chest, back, ...)
+	desc: string;
+	location: string;
+	dateTime: Date;
+}
+
+interface SelfWorkoutComponent extends WorkoutProps {}
+
+interface WorkoutComponent extends WorkoutProps {
+	name: string;
+	age: number;
+	skill: string; // TODO change this to an enum of some sort (e.g. novice, experienced, ...)
+	isFriend: boolean;
 }
---
+++ src/routes/(app)/community/floatingPlus.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Plus from 'virtual:icons/tabler/plus';
+</script>
+
+<div class="absolute bottom-20 w-full flex justify-center">
+	<div
+		class="bg-cyan-400 active:bg-cyan-500 rounded-2xl px-6 py-2 shadow-md/30 active:shadow-sm/75"
+	>
+		<Plus class="text-3xl" />
+	</div>
+</div>
