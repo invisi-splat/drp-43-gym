c488e582776f9292a0d516735178b2a357fbab0e
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2025-06-05 16:46:18 +0100

    implement frontend ui for chat

--- src/routes/(app)/messages/+page.svelte
+++ src/routes/(app)/messages/+page.svelte
@@ -2,10 +2,11 @@
 	import NavbarCompensation from '$lib/components/navbarCompensation.svelte';
 	import Chat from './chat.svelte';
 	import ChatDivider from './chatDivider.svelte';
+	import Header from './header.svelte';
 </script>
 
 <div class="h-dvh w-full overflow-y-scroll bg-gray-100 relative pb-4">
-	<h1 class="top-0 p-4 font-bold text-2xl mb-4 bg-gray-200 h-auto sticky shadow-sm/50">Messages</h1>
+	<Header mainText="Messages" />
 	<Chat />
 	{#each { length: 10 } as _}
 		<ChatDivider />
---
+++ src/routes/(app)/messages/chat/message.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	interface MessageProps {
+		text: string;
+		selfSent?: boolean;
+	}
+
+	let { text, selfSent = false }: MessageProps = $props();
+</script>
+
+<div class="flex {selfSent ? 'justify-end' : 'justify-start'} px-4">
+	<div class="max-w-[85%] rounded-2xl {selfSent ? 'bg-blue-200' : 'bg-gray-200'} p-3 shadow-sm">
+		{text}
+	</div>
+</div>
