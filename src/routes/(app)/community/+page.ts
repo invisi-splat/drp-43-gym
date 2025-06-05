415d164ca4f8e22fdceb30ab3fe01c64cae50190
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2025-06-05 13:48:14 +0100

    remove superfluous import and console logs

--- src/routes/(app)/community/+page.ts
+++ src/routes/(app)/community/+page.ts
@@ -1,10 +1,8 @@
 import { supabase } from '$lib/supabase';
 import type { Tables } from '$lib/types/supabase';
 import type { PageLoad } from './$types';
-import type { WorkoutComponent } from '$lib/types/app';
 
 export const load: PageLoad = async () => {
-	console.log('>>>>>>>>>RUNNING page.ts load functon');
 	const { data, error } = await supabase
 		.from('workouts')
 		.select('id, name, age, skill, "isFriend", regimen, "desc", location, "dateTime"');
@@ -26,6 +24,5 @@ export const load: PageLoad = async () => {
 		dateTime: row.dateTime // Placeholder
 	}));
 
-	console.log('>>>>>>>>returning workouts: ', workouts);
 	return { workouts };
 };
