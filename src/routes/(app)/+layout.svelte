5c7a03607b6efedec472aeb68af16f7b2880604f
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2025-06-15 23:48:45 +0100

    add simple test login system

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import Navbar from './navbar.svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		// Redirect to login if user_id is not set in sessionStorage
+		const user_id = sessionStorage.getItem('user_id');
+
+		if (user_id === null) {
+			goto('/login');
+		}
+	});
 </script>
 
 {@render children()}
