9f843995f4fca9bce6908b3a17e2e69ea3d92ff8
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2025-06-15 17:45:52 +0100

    add error message when viewing from non-mobile viewport

---
+++ src/lib/components/viewportError.svelte
@@ -0,0 +1,5 @@
+<div class="h-lvh w-lvw flex items-center justify-center text-2xl px-20">
+	Thanks for using the app! Currently, there is only styling available for mobile viewports, so
+	please use a mobile device to access the full experience.<br />
+	If you are currently using a mobile device, please rotate your device to landscape mode.
+</div>
